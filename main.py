65c79d5b9e7f2c8db0c40bf9d65071501f6fe508
Yuvraj Shandilya <118852333+yuvshan05@users.noreply.github.com>
2023-10-24 17:06:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("NOW YOU HAVE TO CHOOSE YOUR OWN ADVENTURE \nso where will you go KID\nLEFT or RIGHT")
+direc=input()
+direction=direc.lower()
+if direction=="left":
+  print("now you are Near the river\n what will you do 1)swim\n2)wait\n")
+  riv=input()
+  river=riv.lower()
+  if river=="wait":
+    print("now you are inside a mansion and you have  doors which color door you will open\n1)red\n2)blue")
+    door=input()
+    doors=door.lower()
+    if doors=="red":
+      print("game over")
+    else:
+      print("you are alive and won the treasure.")
+  else:
+    print("game over")
+else:
+  print("game over")
